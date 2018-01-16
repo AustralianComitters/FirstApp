4cb65212bf0beff300312731b70191d516b641da
Perin <perin@rech.com.br>
2018-01-16 09:39:58 -0200

    Created an empty netbeans project

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/* 
+ * FirstApp
+ * CopyRight Rech Informática Ltda. Todos os direitos reservados.
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
